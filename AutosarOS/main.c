990f89194a9f565e37627f9f8070301f39e62693
Pascal Romahn <pascal.romahn@mailbox.org>
2019-06-04 18:53:01 +0200

    Initial commit

---
+++ AutosarOS/main.c
@@ -0,0 +1,18 @@
+/*
+ * AutosarOS.c
+ *
+ * Created: 04.06.2019 18:43:30
+ * Author : Pascal Romahn
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
