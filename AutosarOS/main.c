383951406b44b79e237f3f233754dd3933a2f5f8
Pascal Romahn <pascal.romahn@mailbox.org>
2019-08-12 17:35:39 +0200

    Initial text program:
    * Contains LED blinking
    * Contains system tick
    * Each tick will save and immediatly restore the context
    
    This shows that the basic saving and restoring of the context is correct as no memory corruption is occuring right now

--- AutosarOS/main.c
+++ AutosarOS/main.c
@@ -6,13 +6,76 @@
  */ 
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include "assert.h"
+#include "context.h"
+
+static volatile uint16_t delay = 30;
+
+static volatile uint32_t sysTick = 0;
+
+uint16_t pxCurrentTCB = 0x070F;
+
+void test(void)
+{
+    //PORTB = 0x00;   // keep all LEDs off
+}
 
 
 int main(void)
 {
-    /* Replace with your application code */
+    
+    // Blue D1; White D0
+    DDRD  = 0x00;   // PD as input
+    PORTD = 0xFF;   // enable PU on PD
+    GICR  = 1 << INT0 | 1 << INT1;                              // Enable INT0 and INT1
+    MCUCR = 1 << ISC01 | 0 << ISC00 | 1 << ISC11 | 0 << ISC10;  // Trigger INT0 and INT1 on falling edge
+    
+    DDRB  = 0xFF;   // PB as output
+    PORTB = 0xFF;   // keep all LEDs off
+    
+    TCCR0 = 1 << CS01 | 1 << CS00;      // Enable Timer0 with Prescaler 1024
+    TIMSK |= 1 << TOIE0;                // Enable Overflow Interrupt (Timer0)
+    
+    sei();  //Enable Global Interrupt
+    
     while (1) 
     {
+        static uint8_t t = 0;
+        static int8_t d = 1;
+        PORTB &= ~(1 << t);   // turn LED on
+        _delay_ms(delay);   
+        PORTB |= (1 << t);  // turn LED off 
+        _delay_ms(delay);   
+        
+        t = t + d;
+        if (t == 0)
+            d = 1;
+        else if (t == 7)
+            d = -1;
+        
+       assert(d == 1 || d == -1);
+       assert(t >= 0 && t < 8);
+       
+       test();
+      
     }
 }
 
+ISR(INT0_vect) 
+{
+    delay = delay * 2;
+}
+
+ISR(INT1_vect)
+{
+    delay = delay / 2;
+}
+
+ISR(TIMER0_OVF_vect)
+{
+    save_context();
+    sysTick++;
+    restore_context();
+}
