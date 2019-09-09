b960441f002c8b415ed1cbddfe3944f3c4a49c2e
Pascal Romahn <pascal.romahn@mailbox.org>
2019-09-09 23:48:22 +0200

    Adjusted context assembler functions to work from Task Context instead of ISR context

--- AutosarOS/include/context.h
+++ AutosarOS/include/context.h
@@ -12,14 +12,35 @@
 #define CONTEXT_H_
 
 #include "Task.h"
+#include "TCB.h"
 
+#include <stdint.h>
+
+/**
+ * @brief   Save context of current task
+ */
 extern void save_context();
+
+/**
+ * @brief   Restore context of current task
+ */
 extern void restore_context();
+
+/**
+ * @brief   Initialize context of current task
+ * 
+ * This function is used to initialize the context of the current task.
+ */
 extern void init_context();
 
+/**
+ * @brief   Stack-Pointer saved in current task control block
+ */
 extern uint8_t** pxCurrentTCB;
-extern pTaskFxn pxAdr;
 
-extern volatile uint32_t sysTick;
+/**
+ * @brief   Function pointer to current task function
+ */
+extern pTaskFxn pxAdr;
 
 #endif /* CONTEXT_H_ */
