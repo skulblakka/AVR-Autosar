e73f9879473137b8b2aa20a04bfc5a25d9877e0c
Pascal Romahn <pascal.romahn@mailbox.org>
2019-10-16 19:21:19 +0200

    Add chainTask function

--- AutosarOS/include/Task.h
+++ AutosarOS/include/Task.h
@@ -32,6 +32,22 @@
 extern StatusType OS_ActivateTask(enum tasks_e TaskID);
 
 /**
+ * @brief   Chain task
+ *
+ * The current is transferred from the running state into the suspended state. The specified task will be transferred
+ * into the ready state. The specified task may be identical to the current task.
+ *
+ * @param   TaskID          ID of the task to be chained
+ *
+ * @return  E_OK            No error \n
+ *          E_OS_LIMIT      Too many activations of the task \n
+ *          E_OS_ID         TaskID is invalid \n
+ *          E_OS_RESOURCE   Task still occupies resources \n
+ *          E_OS_CALLLEVEL  Call at interrupt level
+ */
+extern StatusType OS_ChainTask(enum tasks_e TaskID);
+
+/**
  * @brief   Terminate active task
  *
  * The calling task is transferred from the running state into the suspended state.
--- AutosarOS/src/Task.c
+++ AutosarOS/src/Task.c
@@ -26,6 +26,32 @@ extern StatusType OS_ActivateTask(enum tasks_e TaskID)
     return E_OK;
 }
 
+extern StatusType OS_ChainTask(enum tasks_e TaskID)
+{
+    if (isISR) { // TODO: Extended error check
+        return E_OS_CALLLEVEL;
+    }
+
+    if (TaskID >= INVALID_TASK) {
+        return E_OS_ID;
+    }
+
+    // TODO: Check for resources on extended error check
+    TCB_Cfg[currentTask]->curState = SUSPENDED;
+    currentTask = INVALID_TASK;
+
+    // TODO: Handle multiple activations
+    if (TCB_Cfg[TaskID]->curState == SUSPENDED) {
+        TCB_Cfg[TaskID]->curState = PRE_READY;
+    } else if (TCB_Cfg[TaskID]->curState == WAITING) {
+        TCB_Cfg[TaskID]->curState = READY;
+    }
+
+    OS_Schedule();
+
+    return E_OK;
+}
+
 extern StatusType OS_TerminateTask()
 {
     if (isISR) { // TODO: Extended error check
