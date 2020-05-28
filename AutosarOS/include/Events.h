cc48b81b52f814d9e030af1216804f29fb24e81c
Pascal Romahn <pascal.romahn@mailbox.org>
2020-05-28 16:50:57 +0200

    Implement event management

--- AutosarOS/include/Events.h
+++ AutosarOS/include/Events.h
@@ -17,7 +17,77 @@
 #ifndef EVENTS_H_
 #define EVENTS_H_
 
+#include "Types.h"
 
+/**
+ * @brief   Set events
+ *
+ * Set one or several events according to the specified events mask. Other events
+ * remain unchanged.
+ *
+ * The service may be called from an ISR or task level, but not from hook routines.
+ *
+ * If the specified was waiting for at least one of the events specified it will
+ * be transferred to the ready state.
+ *
+ * @param   taskID      ID of the task to set the events for
+ * @param   events      Mask of the events to set
+ *
+ * @return  E_OK            No error \n
+ *          E_OS_ID         TaskID is invalid \n
+ *          E_OS_ACCESS     Referenced task is no extended task \n
+ *          E_OS_STATE      Referenced task is in suspended state
+ */
+extern StatusType Events_SetEvent(enum tasks_e TaskID, EventMaskType events);
+
+/**
+ * @brief   Clear events
+ *
+ * Clear one or several events according to the specified events mask.
+ *
+ * Events can only be cleared by extended tasks. Tasks can only clear their
+ * own events.
+ *
+ * @param   events      Mask of events to clear
+ *
+ * @return  E_OK            No error \n
+ *          E_OS_ACCESS     Call not from extended task \n
+ *          E_OS_CALLLEVEL  Call at interrupt level
+ */
+extern StatusType Events_ClearEvent(EventMaskType events);
+
+/**
+ * @brief   Get events
+ *
+ * Get current event state of task.
+ *
+ * @return  E_OK            No error \n
+ *          E_OS_ID         TaskID is invalid \n
+ *          E_OS_ACCESS     Referenced task is not an extended task \n
+ *          E_OS_STATE      Referenced task is in the suspended state
+ */
+extern StatusType Events_GetEvent(enum tasks_e TaskID, EventMaskRefType events);
+
+/**
+ * @brief   Wait for events
+ *
+ * Wait for events according to the specified events mask. The calling task
+ * is transferred to the waiting state unless at least one of the specified
+ * events is already pending/set.
+ *
+ * If rescheduling takes place the internal resource of the task is released
+ * while the task is in the waiting state.
+ *
+ * This service shall only be called from the extended task owning the events.
+ *
+ * @param   events      Mask of events to wait for
+ *
+ * @return  E_OK            No error \n
+ *          E_OS_ACCESS     Calling task is not an extended task \n
+ *          E_OS_RESOURCE   Calling task occupies resources
+ *          E_OS_CALLLEVEL  Call at interrupt level
+ */
+extern StatusType Events_WaitEvent(EventMaskType events);
 
 
 
--- AutosarOS/src/Events.c
+++ AutosarOS/src/Events.c
@@ -12,3 +12,120 @@
  *              the Free Software Foundation, either version 3 of the License, or
  *              (at your option) any later version.
  */
+
+#include "Events.h"
+#include "Types.h"
+#include "OS.h"
+#include "OCB.h"
+#include "Resource.h"
+
+#include <util/atomic.h>
+
+extern StatusType Events_SetEvent(enum tasks_e TaskID, EventMaskType events)
+{
+    bool taskChanged = false;
+
+    if (TaskID >= INVALID_TASK) { // TODO Extended error check
+        return E_OS_ID;
+    }
+
+    if (TCB_Cfg[TaskID]->taskType != EXTENDED) { // TODO Extended error check
+        return E_OS_ACCESS;
+    }
+
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+        if (TCB_Cfg[TaskID]->curState == SUSPENDED) { // TODO Extended error check
+            return E_OS_STATE;
+        }
+
+        // Set pending events
+        TCB_Cfg[TaskID]->pendingEvents |= (events & TCB_Cfg[TaskID]->events);
+
+        if ((TCB_Cfg[TaskID]->waitEvents & TCB_Cfg[TaskID]->pendingEvents) != 0
+                && TCB_Cfg[TaskID]->curState == WAITING) {
+            // Task was waiting for one of the pending events => transfer to READY state
+            TCB_Cfg[TaskID]->curState = READY;
+
+            taskChanged = true;
+        }
+    }
+
+    if (taskChanged) {
+        // Rescheduling might be required because the task was transferred to the READY state
+        OS_Schedule();
+    }
+
+    return E_OK;
+}
+
+extern StatusType Events_ClearEvent(EventMaskType events)
+{
+    if (isISR) { // TODO Extended error check
+        return E_OS_CALLLEVEL;
+    }
+
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+        if (TCB_Cfg[currentTask]->taskType != EXTENDED) { // TODO Extended error check
+            return E_OS_ACCESS;
+        }
+
+        // Clear specified events
+        TCB_Cfg[currentTask]->pendingEvents &= ~(events & TCB_Cfg[currentTask]->events);
+    }
+
+    return E_OK;
+}
+
+extern StatusType Events_GetEvent(enum tasks_e TaskID, EventMaskRefType events)
+{
+    if (TaskID >= INVALID_TASK) { // TODO Extended error check
+        return E_OS_ID;
+    }
+
+    if (TCB_Cfg[TaskID]->taskType != EXTENDED) { // TODO Extended error check
+        return E_OS_ACCESS;
+    }
+
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+        if (TCB_Cfg[TaskID]->curState == SUSPENDED) { // TODO Extended error check
+            return E_OS_STATE;
+        }
+
+        // Copy pending events
+        *events = TCB_Cfg[currentTask]->pendingEvents;
+    }
+
+    return E_OK;
+}
+
+extern StatusType Events_WaitEvent(EventMaskType events)
+{
+    if (isISR) { // TODO Extended error check
+        return E_OS_CALLLEVEL;
+    }
+
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+        if (TCB_Cfg[currentTask]->taskType != EXTENDED) { // TODO Extended error check
+            return E_OS_ACCESS;
+        }
+
+        if (TCB_Cfg[currentTask]->resourceQueue != NULL) { // TODO Extended error check
+            return E_OS_RESOURCE;
+        }
+
+        // Set wait mask
+        TCB_Cfg[currentTask]->waitEvents = (events & TCB_Cfg[currentTask]->events);
+
+        if ((TCB_Cfg[currentTask]->waitEvents & TCB_Cfg[currentTask]->pendingEvents) == 0) {
+            /* None of the requested events is pending => transfer to WAITING state */
+            TCB_Cfg[currentTask]->curState = WAITING;
+
+            OS_ReleaseInternalResource();
+
+            forceSchedule = true;
+            OS_Schedule();
+        }
+    }
+
+    return E_OK;
+}
