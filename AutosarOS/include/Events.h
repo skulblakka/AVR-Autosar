3ea19cb049bfa92016a78b51b48dc443c451b75c
Pascal Romahn <pascal.romahn@mailbox.org>
2020-05-28 15:40:30 +0200

    Add files for event management

---
+++ AutosarOS/include/Events.h
@@ -0,0 +1,24 @@
+/**
+ * @file
+ *
+ * @brief       Event management
+ *
+ * This contains the operating system control blocks generated during compilation.
+ *
+ * @date        2020-05-28
+ * @author      Pascal Romahn
+ * @copyright   This program is free software: you can redistribute it and/or modify
+ *              it under the terms of the GNU General Public License as published by
+ *              the Free Software Foundation, either version 3 of the License, or
+ *              (at your option) any later version.
+ */
+
+
+#ifndef EVENTS_H_
+#define EVENTS_H_
+
+
+
+
+
+#endif /* EVENTS_H_ */
