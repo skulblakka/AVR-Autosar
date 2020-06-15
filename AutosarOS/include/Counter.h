b74ccdc11bfb37a170156559c0e3ce5d3936b824
Pascal Romahn <pascal.romahn@mailbox.org>
2020-06-15 14:35:00 +0200

    Add files for counter management

---
+++ AutosarOS/include/Counter.h
@@ -0,0 +1,22 @@
+/**
+ * @file
+ *
+ * @brief       Counter management
+ *
+ * @date        2020-06-15
+ * @author      Pascal Romahn
+ * @copyright   This program is free software: you can redistribute it and/or modify
+ *              it under the terms of the GNU General Public License as published by
+ *              the Free Software Foundation, either version 3 of the License, or
+ *              (at your option) any later version.
+ */
+
+
+#ifndef COUNTER_H_
+#define COUNTER_H_
+
+
+
+
+
+#endif /* COUNTER_H_ */
---
+++ AutosarOS/src/Counter.c
@@ -0,0 +1,12 @@
+/**
+ * @file
+ *
+ * @brief       Implementation of counter management
+ *
+ * @date        2020-06-15
+ * @author      Pascal Romahn
+ * @copyright   This program is free software: you can redistribute it and/or modify
+ *              it under the terms of the GNU General Public License as published by
+ *              the Free Software Foundation, either version 3 of the License, or
+ *              (at your option) any later version.
+ */
