c729e29ecba77849c10548223c940be2218caea6
Pascal Romahn <pascal.romahn@mailbox.org>
2020-06-15 14:34:46 +0200

    Fix file documentation

--- AutosarOS/include/Types/CounterTypes.h
+++ AutosarOS/include/Types/CounterTypes.h
@@ -1,15 +1,35 @@
-/*
- * CounterTypes.h
+/**
+ * @file
  *
- * Created: 15.06.2020 14:09:48
- *  Author: pascal
- */ 
+ * @brief       Types used for counter management
+ *
+ * @date        2020-06-15
+ * @author      Pascal Romahn
+ * @copyright   This program is free software: you can redistribute it and/or modify
+ *              it under the terms of the GNU General Public License as published by
+ *              the Free Software Foundation, either version 3 of the License, or
+ *              (at your option) any later version.
+ */
 
 
 #ifndef COUNTERTYPES_H_
 #define COUNTERTYPES_H_
 
+typedef uint64_t TickType;
+typedef TickType* TickRefType;
 
+enum counterType_e {
+    HARDWARE,
+    SOFTWARE
+};
+
+struct counter_s {
+    TickType maxallowedvalue;
+    TickType mincycle;
+    TickType ticksperbase;
+    enum counterType_e type;
+    float secondspertick;
+};
 
 
 
