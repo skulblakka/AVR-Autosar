3f5f5b1ee9c0c1442824c45e640df6b2c3b4f30c
Pascal Romahn <pascal.romahn@mailbox.org>
2020-06-18 17:09:33 +0200

    Add header file for OS API

---
+++ AutosarOS/include/OS_API.h
@@ -0,0 +1,22 @@
+/**
+ * @file
+ *
+ * @brief       API
+ *
+ * @date        2020-06-018
+ * @author      Pascal Romahn
+ * @copyright   This program is free software: you can redistribute it and/or modify
+ *              it under the terms of the GNU General Public License as published by
+ *              the Free Software Foundation, either version 3 of the License, or
+ *              (at your option) any later version.
+ */
+
+
+#ifndef OS_API_H_
+#define OS_API_H_
+
+
+
+
+
+#endif /* OS_API_H_ */
