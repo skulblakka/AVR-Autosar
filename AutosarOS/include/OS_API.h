fb17fb35f328efa4c276711f977e865df0d9dce4
Pascal Romahn <pascal.romahn@mailbox.org>
2020-06-18 17:36:45 +0200

    Create OS API

--- AutosarOS/include/OS_API.h
+++ AutosarOS/include/OS_API.h
@@ -15,8 +15,71 @@
 #ifndef OS_API_H_
 #define OS_API_H_
 
+#include "Types.h"
 
+/************************************************************************/
+/* ALARM MANAGEMENT                                                     */
+/************************************************************************/
+#include "Alarm.h"
 
+#define GetAlarmBase                Alarm_GetAlarmBase
+#define GetAlarm                    Alarm_GetAlarm
+#define SetRelAlarm                 Alarm_SetRelAlarm
+#define SetAbsAlarm                 Alarm_SetAbsAlarm
+#define CancelAlarm                 Alarm_CancelAlarm
 
+/************************************************************************/
+/* COUNTER MANAGEMENT                                                   */
+/************************************************************************/
+#include "Counter.h"
+
+#define IncrementCounter            Counter_IncrementCounter
+#define GetCounterValue             Counter_GetCounterValue
+#define GetElapsedValue             Counter_GetElapsedValue
+
+/************************************************************************/
+/* EVENT MANAGEMENT                                                     */
+/************************************************************************/
+#include "Events.h"
+
+#define SetEvent                    Events_SetEvent
+#define ClearEvent                  Events_ClearEvent
+#define GetEvent                    Events_GetEvent
+#define WaitEvent                   Events_WaitEvent
+
+/************************************************************************/
+/* OPERATING SYSTEM CONTROL                                             */
+/************************************************************************/
+#include "OS.h"
+
+#define StartOS                     OS_StartOS
+#define ShutdownOS                  OS_ShutdownOS
+#define GetActiveApplicationMode    OS_GetActiveApplicationMode
+#define EnableAllInterrupts         OS_EnableAllInterrupts
+#define DisableAllInterrupts        OS_DisableAllInterrupts
+#define ResumeAllInterrupts         OS_ResumeAllInterrupts
+#define SuspendAllInterrupts        OS_SuspendAllInterrupts
+#define ResumeOSInterrupts          OS_ResumeOSInterrupts
+#define SuspendOSInterrupts         OS_SuspendOSInterrupts
+
+/************************************************************************/
+/* RESOURCE MANAGEMENT                                                  */
+/************************************************************************/
+#include "Resource.h"
+
+#define GetResource                 Resource_GetResource
+#define ReleaseResource             Resource_ReleaseResource
+
+/************************************************************************/
+/* TASK MANAGEMENT                                                      */
+/************************************************************************/
+#include "Task.h"
+
+#define ActivateTask                Task_ActivateTask
+#define ChainTask                   Task_ChainTask
+#define TerminateTask               Task_TerminateTask
+#define Schedule                    Task_Schedule
+#define GetTaskID                   Task_GetTaskID
+#define GetTaskState                Task_GetTaskState
 
 #endif /* OS_API_H_ */
