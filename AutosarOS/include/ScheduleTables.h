0e83a3da725d86317a0168cba3b86f50c46ed7cd
Pascal Romahn <pascal.romahn@mailbox.org>
2020-06-26 17:49:18 +0200

    Implement functions for schedule table management

--- AutosarOS/include/ScheduleTables.h
+++ AutosarOS/include/ScheduleTables.h
@@ -15,8 +15,112 @@
 #ifndef SCHEDULETABLES_H_
 #define SCHEDULETABLES_H_
 
+#include "Types.h"
 
+/**
+ * @brief   Start schedule table from relative offset
+ *
+ * Start schedule table from relative offset. The first expiry point will be executed in
+ * offset + offset of first expiry point ticks of the underlying counter.
+ *
+ * Offset must be greater than zero and smaller or equal MaxAllowedValue of the underlying counter.
+ *
+ * @param   scheduleTableID         ID of the schedule table to start
+ * @param   offset                  Offset in Ticks before the schedule table is executed
+ *
+ * @return  E_OK                    No error \n
+ *          E_OS_ID                 Invalid schedule table ID \n
+ *          E_OS_VALUE              Value of offset is invalid \n
+ *          E_OS_STATE              Schedule table was already started
+ */
+extern StatusType ScheduleTable_StartScheduleTableRel(ScheduleTableType scheduleTableID, TickType offset);
 
+/**
+ * @brief   Start schedule table from absolute start
+ *
+ * Start the schedule table from an absolute starting point. The first expiry point will be
+ * executed at start + offset of first expiry point tick value of the underlaying counter.
+ *
+ * Start must be below or equal MaxAllowedValue of the underlying counter.
+ *
+ * @param   scheduleTableID         ID of the schedule table to start
+ * @param   start                   Start value in ticks
+ *
+ * @return  E_OK                    No error \n
+ *          E_OS_ID                 Invalid schedule table ID \n
+ *          E_OS_VALUE              Value of start is invalid \n
+ *          E_OS_STATE              Schedule table was already started
+ */
+extern StatusType ScheduleTable_StartScheduleTableAbs(ScheduleTableType scheduleTableID, TickType start);
 
+/**
+ * @brief   Stop schedule table
+ *
+ * Stop execution of the schedule table.
+ *
+ * @param   scheduleTableID         ID of the schedule table to stop
+ *
+ * @return  E_OK                    No error \n
+ *          E_OS_ID                 Invalid schedule table ID \n
+ *          E_OS_NOFUNC             Schedule table was already stopped
+ */
+extern StatusType ScheduleTable_StopScheduleTable(ScheduleTableType scheduleTableID);
+
+/**
+ * @brief   Queue schedule table
+ *
+ * Queue schedule table to be executed after the current schedule table finishes
+ * execution. If a there is already a next schedule table configured it will be replaced.
+ *
+ * @param   scheduleTableID_from    ID of current schedule table
+ * @param   scheduleTableID_to      ID of schedule table to be queued
+ *
+ * @return  E_OK                    No error \n
+ *          E_OS_ID                 Invalid schedule table IDs \n
+ *          E_OS_NOFUNC             Current schedule table is not started \n
+ *          E_OS_STATE              Schedule table to be queues is not stopped
+ */
+extern StatusType ScheduleTable_NextScheduleTable(ScheduleTableType scheduleTableID_from,
+        ScheduleTableType scheduleTableID_to);
+
+/**
+ * @brief   Get schedule table status
+ *
+ * Get current status of a schedule table.
+ *
+ * @param   scheduleTableID         ID of schedule table
+ * @param   scheduleStatus          Reference to status
+ *
+ * @return  E_OK                    No error \n
+ *          E_OS_ID                 Invalid schedule table ID
+ */
+extern StatusType ScheduleTable_GetScheduleTableStatus(ScheduleTableType scheduleTableID,
+        ScheduleTableStatusRefType scheduleStatus);
+
+/**
+ * @brief   Handle tick
+ *
+ * Handles a tick of the specified counter. This will increment all associated schedule
+ * tables and handle expiry points if necessary.
+ *
+ * @param   counter                 ID of the counter that triggered the tick
+ */
+extern void ScheduleTable_handleTick(CounterType counter);
+
+/**
+ * @brief   Handle system tick
+ *
+ * Calls ScheduleTable_handleTick() where counter euqals SYSTEM_COUNTER. This only exists
+ * to simplify the function call within the SysTick interrupt.
+ */
+extern void ScheduleTable_handleSysTick(void);
+
+/**
+ * @brief   Startup function for schedule table management
+ *
+ * This function starts all schedule tables configured as autostart and handles expiry
+ * points if necessary.
+ */
+extern void ScheduleTable_startup(void);
 
 #endif /* SCHEDULETABLES_H_ */
--- AutosarOS/src/ScheduleTables.c
+++ AutosarOS/src/ScheduleTables.c
@@ -10,3 +10,308 @@
  *              the Free Software Foundation, either version 3 of the License, or
  *              (at your option) any later version.
  */
+
+#include "ScheduleTables.h"
+#include "Counter.h"
+#include "Task.h"
+#include "Events.h"
+
+#include "OCB.h"
+#include "OS.h"
+
+#include "assert.h"
+
+#include <util/atomic.h>
+
+/************************************************************************/
+/* STATIC FUNCTIONS                                                     */
+/************************************************************************/
+/**
+ * @brief   Handle expiry point
+ *
+ * Handle an expiry point and perform the configured actions.
+ *
+ * @param   expiryPoint         Pointer to expiry point to handle
+ */
+static void ScheduleTable_handleExpiryPoint(const volatile struct scheduleTableExpiryPoint_s* expiryPoint)
+{
+    /* Set initial pointers */
+    const volatile struct scheduleTableExpiryActionTask_s* actionTask = expiryPoint->taskActionList;
+    const volatile struct scheduleTableExpiryActionEvent_s* actionEvent = expiryPoint->eventActionList;
+
+    /* Handle task activations */
+    while (actionTask != NULL) {
+        Task_ActivateTask(actionTask->task);
+
+        actionTask = actionTask->next;
+    }
+
+    /* Handle events */
+    while (actionEvent != NULL) {
+        Events_SetEvent(actionEvent->task, actionEvent->event);
+
+        actionEvent = actionEvent->next;
+    }
+}
+
+/**
+ * @brief   Handle start of schedule table
+ *
+ * This handles the start of a schedule table. It will reset the current tick and set the
+ * current state to #SCHEDULETABLE_RUNNING.
+ *
+ * The first expiry point will be handled if its offset equals zero. However scheduling
+ * is blocked during this.
+ *
+ * @warning Interrupts must be disabled during this function.
+ *
+ * @param   scheduleTableID     Schedule table to be started
+ */
+static void ScheduleTable_handleScheduleTableStart(ScheduleTableType scheduleTableID)
+{
+    ScheduleTable_Cfg[scheduleTableID]->currentState = SCHEDULETABLE_RUNNING;
+    ScheduleTable_Cfg[scheduleTableID]->currentTick = 0;
+
+    /* Handle first expiry point if it has zero offset */
+    if (ScheduleTable_Cfg[scheduleTableID]->expiryPointList.offset == 0) {
+        bool savedBlock = blockScheduling;
+        blockScheduling = true;
+        ScheduleTable_handleExpiryPoint(&ScheduleTable_Cfg[scheduleTableID]->expiryPointList);
+        blockScheduling = savedBlock;
+    }
+}
+
+/************************************************************************/
+/* EXTERN FUNCTIONS                                                     */
+/************************************************************************/
+extern StatusType ScheduleTable_StartScheduleTableRel(ScheduleTableType scheduleTableID, TickType offset)
+{
+    OS_SET_ERROR_INFO2(OSServiceId_StartScheduleTableRel, &scheduleTableID, sizeof(scheduleTableID), &offset,
+            sizeof(offset));
+
+    if (OS_EXTENDED && scheduleTableID >= INVALID_SCHEDULETABLE) {
+        OS_CALL_ERROR_HOOK();
+
+        return E_OS_ID;
+    }
+
+    if (OS_EXTENDED && (offset == 0
+                    || offset > Counter_Cfg[ScheduleTable_Cfg[scheduleTableID]->counter]->maxallowedvalue -
+                    ScheduleTable_Cfg[scheduleTableID]->expiryPointList.offset)) {
+        OS_CALL_ERROR_HOOK();
+
+        return E_OS_VALUE;
+    }
+
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+        if (ScheduleTable_Cfg[scheduleTableID]->currentState != SCHEDULETABLE_STOPPED) {
+            OS_CALL_ERROR_HOOK();
+
+            return E_OS_STATE;
+        }
+
+        ScheduleTable_Cfg[scheduleTableID]->currentTick = 0 - offset;
+        ScheduleTable_Cfg[scheduleTableID]->currentState = SCHEDULETABLE_RUNNING;
+    }
+
+    return E_OK;
+}
+
+extern StatusType ScheduleTable_StartScheduleTableAbs(ScheduleTableType scheduleTableID, TickType start)
+{
+    OS_SET_ERROR_INFO2(OSServiceId_StartScheduleTableAbs, &scheduleTableID, sizeof(scheduleTableID), &start,
+            sizeof(start));
+
+    if (OS_EXTENDED && scheduleTableID >= INVALID_SCHEDULETABLE) {
+        OS_CALL_ERROR_HOOK();
+
+        return E_OS_ID;
+    }
+
+    if (OS_EXTENDED && start > Counter_Cfg[ScheduleTable_Cfg[scheduleTableID]->counter]->maxallowedvalue) {
+        OS_CALL_ERROR_HOOK();
+
+        return E_OS_VALUE;
+    }
+
+    TickType currentTick;
+    Counter_GetCounterValue(ScheduleTable_Cfg[scheduleTableID]->counter, &currentTick);
+
+    if (start > currentTick) {
+        start -= currentTick;
+    } else if (start < currentTick) {
+        start -= Counter_Cfg[ScheduleTable_Cfg[scheduleTableID]->counter]->maxallowedvalue - currentTick;
+    } else {
+        start = Counter_Cfg[ScheduleTable_Cfg[scheduleTableID]->counter]->maxallowedvalue;
+    }
+
+    return ScheduleTable_StartScheduleTableRel(scheduleTableID, start);
+}
+
+extern StatusType ScheduleTable_StopScheduleTable(ScheduleTableType scheduleTableID)
+{
+    OS_SET_ERROR_INFO1(OSServiceId_StopScheduleTable, &scheduleTableID, sizeof(scheduleTableID));
+
+    if (OS_EXTENDED && scheduleTableID >= INVALID_SCHEDULETABLE) {
+        OS_CALL_ERROR_HOOK();
+
+        return E_OS_ID;
+    }
+
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+        if (ScheduleTable_Cfg[scheduleTableID]->currentState == SCHEDULETABLE_STOPPED) {
+            OS_CALL_ERROR_HOOK();
+
+            return E_OS_NOFUNC;
+        }
+
+        ScheduleTable_Cfg[scheduleTableID]->currentState = SCHEDULETABLE_STOPPED;
+    }
+
+    return E_OK;
+}
+
+extern StatusType ScheduleTable_NextScheduleTable(ScheduleTableType scheduleTableID_from,
+        ScheduleTableType scheduleTableID_to)
+{
+    OS_SET_ERROR_INFO2(OSServiceId_NextScheduleTable, &scheduleTableID_from, sizeof(scheduleTableID_from),
+            &scheduleTableID_to, sizeof(scheduleTableID_to));
+
+    if (OS_EXTENDED && (scheduleTableID_from >= INVALID_SCHEDULETABLE || scheduleTableID_to >= INVALID_SCHEDULETABLE)) {
+        OS_CALL_ERROR_HOOK();
+
+        return E_OS_ID;
+    }
+
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+        if (ScheduleTable_Cfg[scheduleTableID_from]->currentState == SCHEDULETABLE_STOPPED
+                || ScheduleTable_Cfg[scheduleTableID_from]->currentState == SCHEDULETABLE_NEXT) {
+            OS_CALL_ERROR_HOOK();
+
+            return E_OS_NOFUNC;
+        }
+
+        if (ScheduleTable_Cfg[scheduleTableID_to]->currentState != SCHEDULETABLE_STOPPED) {
+            OS_CALL_ERROR_HOOK();
+
+            return E_OS_NOFUNC;
+        }
+
+        if (ScheduleTable_Cfg[scheduleTableID_from]->next != INVALID_SCHEDULETABLE) {
+            ScheduleTable_Cfg[ScheduleTable_Cfg[scheduleTableID_from]->next]->currentState = SCHEDULETABLE_STOPPED;
+        }
+
+        ScheduleTable_Cfg[scheduleTableID_from]->next = scheduleTableID_to;
+        ScheduleTable_Cfg[scheduleTableID_to]->currentState = SCHEDULETABLE_NEXT;
+    }
+
+    return E_OK;
+}
+
+extern StatusType ScheduleTable_GetScheduleTableStatus(ScheduleTableType scheduleTableID,
+        ScheduleTableStatusRefType scheduleStatus)
+{
+    OS_SET_ERROR_INFO2(OSServiceId_GetScheduleTableStatus, &scheduleTableID, sizeof(scheduleTableID), &scheduleStatus,
+            sizeof(scheduleStatus));
+
+    if (OS_EXTENDED && scheduleTableID >= INVALID_SCHEDULETABLE) {
+        OS_CALL_ERROR_HOOK();
+
+        return E_OS_ID;
+    }
+
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+        *scheduleStatus = ScheduleTable_Cfg[scheduleTableID]->currentState;
+    }
+
+    return E_OK;
+}
+
+extern void ScheduleTable_handleTick(CounterType counter)
+{
+    for (uint8_t i = 0; i < SCHEDULETABLE_COUNT; i++) {
+        if (ScheduleTable_Cfg[i]->counter != counter) {
+            // Only evaluate schedule table if the associated counter was changed
+            continue;
+        }
+
+        ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+            if (ScheduleTable_Cfg[i]->currentState != SCHEDULETABLE_RUNNING) {
+                continue;
+            }
+
+            ScheduleTable_Cfg[i]->currentTick += 1;
+
+            // Get first expiry point
+            const volatile struct scheduleTableExpiryPoint_s* expiryPoint = &ScheduleTable_Cfg[i]->expiryPointList;
+
+            while (expiryPoint != NULL) {
+                if (expiryPoint->offset == ScheduleTable_Cfg[i]->currentTick) {
+                    ScheduleTable_handleExpiryPoint(expiryPoint);
+
+                    // Only one expiry point per offset per schedule table allowed
+                    break;
+                } else if (expiryPoint->offset > ScheduleTable_Cfg[i]->currentTick) {
+                    // Reached upcoming expiry points
+                    break;
+                }
+
+                // Check if we reached to end of the schedule table
+                if (expiryPoint->next == NULL) {
+                    break;
+                }
+
+                // Get next expiry point
+                expiryPoint = expiryPoint->next;
+            }
+
+            /* Handle end of schedule table */
+            if (expiryPoint->next == NULL) {
+                if (ScheduleTable_Cfg[i]->finalDelay == 0
+                        || ScheduleTable_Cfg[i]->finalDelay == ScheduleTable_Cfg[i]->currentTick - expiryPoint->offset) {
+                    if (ScheduleTable_Cfg[i]->cyclic) {
+                        // Schedule table is cyclic => restart it
+                        ScheduleTable_handleScheduleTableStart(i);
+                    } else {
+                        // Stop schedule table
+                        ScheduleTable_Cfg[i]->currentState = SCHEDULETABLE_STOPPED;
+
+                        if (ScheduleTable_Cfg[i]->next != INVALID_SCHEDULETABLE) {
+                            if (i < ScheduleTable_Cfg[i]->next
+                                    && ScheduleTable_Cfg[ScheduleTable_Cfg[i]->next]->counter == counter) {
+                                /* Start queued schedule table (will be handled within this tick evaluation) */
+                                ScheduleTable_Cfg[ScheduleTable_Cfg[i]->next]->currentState = SCHEDULETABLE_RUNNING;
+                                ScheduleTable_Cfg[ScheduleTable_Cfg[i]->next]->currentTick = -1;
+                            } else {
+                                // Start queued schedule table and handle initial expiry point if necessary
+                                ScheduleTable_handleScheduleTableStart(ScheduleTable_Cfg[i]->next);
+                            }
+
+                            // Reset queued schedule table field
+                            ScheduleTable_Cfg[i]->next = INVALID_SCHEDULETABLE;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+extern void ScheduleTable_handleSysTick(void)
+{
+    ScheduleTable_handleTick(SYSTEM_COUNTER);
+}
+
+extern void ScheduleTable_startup(void)
+{
+    if (SCHEDULETABLE_COUNT > 0) {
+        assert(SCHEDULETABLE_COUNT <= UINT8_MAX);
+
+        for (uint8_t i = 0; i < SCHEDULETABLE_COUNT; i++) {
+            /* Start all schedule tables configured as autostart */
+            if (ScheduleTable_Cfg[i]->autoStart == true) {
+                ScheduleTable_handleScheduleTableStart(i);
+            }
+        }
+    }
+}
