8c648624614e6ccd3ce62fd1ab45675940337d75
Pascal Romahn <pascal.romahn@mailbox.org>
2020-11-27 22:15:20 +0100

    Add a bit more documentation the the context switching functions

--- AutosarOS/include/context.h
+++ AutosarOS/include/context.h
@@ -32,11 +32,86 @@ extern pTaskFxn ptrCurrentFxnAddr;
 
 /**
  * @brief   Save context of current task
+ *
+ * This function is used to save the current task context by saving the current processor state
+ * onto the stack. The stack pointer is saved into the task context #ptrCurrentStack points to.
+ *
+ * Upon entry the current stack pointer points to the return address to OS_ScheduleC() and the
+ * return address to the caller of OS_ScheduleC().
+ * ```
+ * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+ * +  Return to   | Return to caller | ...
+ * + OS_ScheduleC | of OS_ScheduleC  |
+ * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+ * ^
+ * Stack Pointer
+ * ```
+ *
+ * The return address to OS_ScheduleC() is removed from the stack. Afterwards all registers are copied
+ * onto the stack and the stack pointer is saved into #ptrCurrentStack. Afterwards the return address to
+ * OS_ScheduleC() is again added to the stack.
+ * ```
+ * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+ * +  Return to   | R31 | .. | R2 | R1 | SREG | R0 | Return to caller | ...
+ * + OS_ScheduleC |     |    |    |    |      |    | of OS_ScheduleC  |
+ * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+ * ^              ^
+ * Stack Pointer  *ptrCurrentStack
+ * ```
+ *
+ * After the functions returns the current stack looks like this:
+ * ```
+ * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+ * + R31 | .. | R2 | R1 | SREG | R0 | Return to caller | ...
+ * +     |    |    |    |      |    | of OS_ScheduleC  |
+ * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+ * ^
+ * Stack Pointer
+ * *ptrCurrentStack
+ * ```
  */
 extern void save_context(void);
 
 /**
  * @brief   Restore context of current task
+ *
+ * This function is used to restore the current task context by restoring the processor state
+ * from the stack of the currently selected task.
+ *
+ * Upon entry both task stacks look like this. It is assumed that the old/previous task was saved via
+ * save_context() beforehand.
+ * ```
+ *       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+ * Old   +  Return to   | R31 | .. | R2 | R1 | SREG | R0 | Return to caller | ...
+ * task  + OS_ScheduleC |     |    |    |    |      |    | of OS_ScheduleC  |
+ *       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+ *       ^
+ *       Stack Pointer
+ *
+ *       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+ * New   + R31 | .. | R2 | R1 | SREG | R0 | Return to caller | ...
+ * task  +     |    |    |    |      |    | of OS_ScheduleC  |
+ *       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+ *       ^
+ *       *ptrCurrentStack
+ * ```
+ *
+ * The return address to OS_ScheduleC() is removed from the current stack and the stack pointer is
+ * changed to the value #ptrCurrentStack points to. Afterwards the processor state is restored and the
+ * return address to OS_ScheduleC() is again added to the new stack.
+ * ```
+ *       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+ * Old   + R31 | .. | R2 | R1 | SREG | R0 | Return to caller | ...
+ * task  +     |    |    |    |      |    | of OS_ScheduleC  |
+ *       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+ *
+ *       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+ * New   +  Return to   | Return to caller | ...
+ * task  + OS_ScheduleC | of OS_ScheduleC  |
+ *       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+ *       ^
+ *       Stack Pointer
+ * ```
  */
 extern void restore_context(void);
 
@@ -44,6 +119,28 @@ extern void restore_context(void);
  * @brief   Initialize context of current task
  *
  * This function is used to initialize the context of the current task.
+ *
+ * Upon entry the current stack pointer points to the return address to OS_ScheduleC().
+ * ```
+ *       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+ * Old   +  Return to   | Return to caller | ...
+ * task  + OS_ScheduleC | of OS_ScheduleC  |
+ *       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+ *       ^
+ *       Stack Pointer
+ * ```
+ *
+ * The return address to OS_ScheduleC() is removed from the current stack and the stack pointer is changed to
+ * the value #ptrCurrentStack points to. Afterwards the value #ptrCurrentFxnAddr points to and the return address to
+ * OS_ScheduleC() are added to the stack.
+ * ```
+ *       +-+-+-+-+-+-+-+-+-+-+-+-+-+-
+ * New   +  Return to   | Return to |
+ * task  + OS_ScheduleC |  taskFxn  |
+ *       +-+-+-+-+-+-+-+-+-+-+-+-+-+-
+ *       ^                          ^
+ *       Stack Pointer              *ptrCurrentStack
+ * ```
  */
 extern void init_context(void);
 
